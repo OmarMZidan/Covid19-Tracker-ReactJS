8ce4c953bb359ec63a880f4dc8fc985e66d0e3a5
OmarMZidan <omarzidan87@outlook.com>
2021-01-06 13:22:23 +0100

    add header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import { FormControl, Select } from "@material-ui/core";
 import "./App.css";
 
 function App() {
-  return <div className="app"></div>;
+  return (
+    <div className="app">
+      <h1>Covid-19 Tracker </h1>
+      <FormControl className="app__dropdown">
+        <Select variant="outlined" value="abc"></Select>
+      </FormControl>
+    </div>
+  );
 }
 
 export default App;
