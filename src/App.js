f55286481ff52f00fae1baaacc2c943a3c08f928
OmarMZidan <omarzidan87@outlook.com>
2021-01-07 16:43:43 +0100

    add a basic layout - srtucture & components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,74 @@
-import { FormControl, Select } from "@material-ui/core";
+import React, { useEffect, useState } from "react";
+
+import {
+  FormControl,
+  Select,
+  MenuItem,
+  Card,
+  CardContent,
+} from "@material-ui/core";
 import "./App.css";
+import InfoBox from "./components/InfoBox";
+import Map from "./components/Map";
 
 function App() {
+  const [countries, setCountries] = useState([]);
+  const [country, setCountry] = useState("worldwide");
+
+  useEffect(() => {
+    //async => send a request, wait for it, do something with info
+
+    const getCountriesData = async () => {
+      await fetch(" https://disease.sh/v3/covid-19/countries")
+        .then((response) => response.json())
+        .then((data) => {
+          const countries = data.map((country) => ({
+            name: country.country,
+            value: country.countryInfo.iso2,
+          }));
+          setCountries(countries);
+        });
+    };
+    getCountriesData();
+  }, []);
+
+  const onCountryChange = async (event) => {
+    const countryCode = event.target.value;
+    setCountry(countryCode);
+  };
   return (
     <div className="app">
-      <h1>Covid-19 Tracker </h1>
-      <FormControl className="app__dropdown">
-        <Select variant="outlined" value="abc"></Select>
-      </FormControl>
+      <div className="app__left">
+        <div className="app__header">
+          <h1>Covid-19 Tracker </h1>
+          <FormControl className="app__dropdown">
+            <Select
+              variant="outlined"
+              value={country}
+              onChange={onCountryChange}
+            >
+              <MenuItem value="worldwide">Worldwide</MenuItem>
+              {countries.map((country) => (
+                <MenuItem value={country.value}>{country.name}</MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+        </div>
+
+        <div className="app__stats">
+          <InfoBox title="Coronavirus Cases" cases="1987" total="2021" />
+          <InfoBox title="Recovered" cases="1987" total="2021" />
+          <InfoBox title="Deaths" cases="1987" total="2021" />
+        </div>
+
+        <Map />
+      </div>
+      <Card className="app__right">
+        <CardContent>
+          <h3>live Cases by Country</h3>
+          <h3>Worldwide new cases</h3>
+        </CardContent>
+      </Card>
     </div>
   );
 }
